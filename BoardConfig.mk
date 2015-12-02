6732aeb59afe514b0516b7c6025b9c596da71653
Nick <nickvanbokhorst@hotmail.com>
2015-12-02 17:53:41 +0100

    add some additional optimizations
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,3 +40,18 @@ include device/asus/grouper/BoardConfigCommon.mk
 
 TARGET_RECOVERY_FSTAB = device/asus/grouper/fstab.grouper
 
+TARGET_DISABLE_ARM_PIE := true
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=hardfp -gtoggle -s -DNDEBUG -march=armv7-a -mthumb -O2 -funroll-loops -mimplicit-it=always -mno-warn-deprecated -mauto-it --disable-docs -mtls-dialect=gnu2 --param l1-cache-size=32 --param l1-cache-line-size=32 --param l2-cache-size=1024 --param simultaneous-prefetches=6 --param prefetch-latency=400 -mvectorize-with-neon-quad
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=hardfp -gtoggle -s -DNDEBUG -O2 -funroll-loops -mthumb -march=armv7-a -mimplicit-it=always -mno-warn-deprecated -mauto-it --disable-docs -mtls-dialect=gnu2 --param l1-cache-size=32 --param l1-cache-line-size=32 --param l2-cache-size=1024 --param simultaneous-prefetches=6 --param prefetch-latency=400 -mvectorize-with-neon-quad
+TARGET_BOOTANIMATION_PRELOAD := true 
+BOARD_SKIP_ANDROID_DOC_BUILD := true
+DISABLE_DROIDDOC := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := false
+TARGET_ENABLE_NON_PIE_SUPPORT := true
+#WITH_DEXPREOPT := true
+HWUI_COMPILE_FOR_PERF := true
+$(call add-product-dex-preopt-module-config,services,--compiler-filter=everything)
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dex2oat-flags=--no-watch-dog \
+dalvik.vm.dex2oat-filter=everything \
+dalvik.vm.image-dex2oat-filter=everything
