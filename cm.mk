b37b442ca76dd0b1a6640ff20c27b72f59026543
Aaron Peromsik <aaron@peromsik.net>
2016-11-26 19:25:50 -0500

    update for CM removal of enhanced NFC

--- cm.mk
+++ cm.mk
@@ -8,9 +8,6 @@ TARGET_SCREEN_WIDTH := 800
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
