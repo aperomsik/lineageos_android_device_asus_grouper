7fb5270a93c0367dd8c8e6ac46b315e042504d6f
Aaron Peromsik <aaron@peromsik.net>
2017-01-01 20:58:10 -0500

    cm -> lineage

--- lineage.mk
+++ lineage.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := Nexus7
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 800
 
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
