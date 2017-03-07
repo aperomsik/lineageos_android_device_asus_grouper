3cc57c282b30b26aca0049306f460b67e0904226
AndDiSa <anddisa@googlemail.com>
2017-03-07 19:33:16 +0100

    power hal: keep 2 cores min in interactive mode

--- power/power.c
+++ power/power.c
@@ -33,6 +33,7 @@
 #include <hardware/power.h>
 
 #define BOOST_PATH      "/sys/devices/system/cpu/cpufreq/interactive/boost"
+#define INTERACTIVE_KEEP_MIN_CPU      "/sys/devices/system/cpu/cpufreq/interactive/min_core_keep"
 #define UEVENT_MSG_LEN 2048
 #define TOTAL_CPUS 4
 #define RETRY_TIME_CHANGING_FREQ 20
@@ -194,6 +195,7 @@ static void grouper_power_init( __attribute__((unused)) struct power_module *mod
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost", "1");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/io_is_busy", "1");
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq", "860000");
+    sysfs_write(INTERACTIVE_KEEP_MIN_CPU, "4");
     uevent_init();
 }
 
@@ -206,6 +208,7 @@ static void grouper_power_set_interactive(__attribute__((unused)) struct power_m
 		sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/core_lock_count", "2");
 		sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost", "1");
 		sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/io_is_busy", "1");
+    		sysfs_write(INTERACTIVE_KEEP_MIN_CPU, "2");
 	}
 	else {
 		sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load", "85");
@@ -213,6 +216,7 @@ static void grouper_power_set_interactive(__attribute__((unused)) struct power_m
 		sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/core_lock_count", "0");
 		sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost", "0");
 		sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/io_is_busy", "0");
+    		sysfs_write(INTERACTIVE_KEEP_MIN_CPU, "0");
 	}
 }
 
